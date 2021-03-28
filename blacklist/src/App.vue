dca5bff329e934b5248dcb23ff2524e4cee8146a
Andrey <tinymail8@gmail.com>
2021-03-28 16:37:22 +0300

    repair yandex metrika

--- blacklist/src/App.vue
+++ blacklist/src/App.vue
@@ -5,13 +5,11 @@
     <transition name="fade" mode="out-in">
       <router-view class="content-block" />
     </transition>
-    <Mertrika />
   </div>
 </template>
 
 <script>
 import Header from "Common/Header";
-import Mertrika from "Common/YaMetrika";
 import { mapGetters } from "vuex";
 
 export default {
@@ -29,8 +27,7 @@ export default {
     }
   },
   components: {
-    Header,
-    Mertrika
+    Header
   }
 };
 </script>
